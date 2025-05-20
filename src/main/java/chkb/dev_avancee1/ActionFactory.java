a4f637f61bc6cc09e5dd1dcdca7fa7c0c68e5bcf
Chak <444chak@gmail.com>
2025-05-20 12:59:43 +0200

    Refactor ActionFactory to load actions from XML configuration

--- src/main/java/chkb/dev_avancee1/ActionFactory.java
+++ src/main/java/chkb/dev_avancee1/ActionFactory.java
@@ -1,6 +1,9 @@
 package chkb.dev_avancee1;
 
-import chkb.dev_avancee1.action.*;
+import chkb.dev_avancee1.action.Action;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,12 +11,26 @@ public class ActionFactory {
     private static final Map<String, Class<? extends Action>> actions = new HashMap<>();
 
     static {
-        actions.put("index.htm", IndexAction.class);
-        actions.put("login.do", LoginAction.class);
-        actions.put("logout.do", LogoutAction.class);
-        actions.put("updateAttributes.do", UpdateAttributesAction.class);
-        actions.put("validateAttributes.do", ValidateAttributesAction.class);
-        actions.put("start.do", StartAction.class);
+        try {
+            InputStream is = ActionFactory.class.getClassLoader().getResourceAsStream("actions.xml");
+            if (is != null) {
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(is);
+                NodeList nList = doc.getElementsByTagName("action");
+                for (int i = 0; i < nList.getLength(); i++) {
+                    Element elem = (Element) nList.item(i);
+                    String name = elem.getAttribute("name");
+                    String className = elem.getAttribute("class");
+                    Class<?> clazz = Class.forName(className);
+                    if (Action.class.isAssignableFrom(clazz)) {
+                        actions.put(name, (Class<? extends Action>) clazz);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static Action getAction(String actionName) {
